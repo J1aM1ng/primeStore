002941659569a8d8a8c2d9ccdd864759f99b3dbf
determinedashing <2605379072@qq.com>
2020-07-14 15:24:09 +0800

    remove the file named src

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-let var1 = document.getElementById("button01");
-var1.onclick = function() {
-    alert('请登录后进行操作！');
-}
-
-let var2 = document.getElementById("button02");
-var2.onclick = function() {
-    alert('请登录后进行操作！');
-}
